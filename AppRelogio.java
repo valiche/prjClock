f54a09d339998b1de7d3edebcdf2cea549d229e4
ISABELE VALICHELLI DE MATOS <987266071@itaud.des.ihf>
2020-07-31 16:54:05 -0300

    commit inic

---
+++ AppRelogio.java
@@ -0,0 +1,10 @@
+public class AppRelogio {
+    public static void main(String[] args) {
+        Relogio clock;
+        clock=new Relogio();
+
+        clock.setHora();
+        clock.getHora();
+        System.out.println(clock.getHora());
+    }
+}
---
+++ Relogio.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Relogio {
+    int hora = 0, min = 0, seg = 0;
+
+    public void setHora() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Digite hora minuto segundo:");
+        hora = in.nextInt();
+        min = in.nextInt();
+        seg = in.nextInt();
+        for (; hora <=24; hora++) {
+            for (; min < 60; min++) {
+                for (; seg < 60; seg++){
+                    //System.out.printf("%d:%d:%d\n", hora, min, seg);
+                    //return hora + ":" + min + ":" + seg;
+                }
+                    seg=0;
+            }
+            min=0;
+        }
+        hora=0;
+        //System.out.printf("%d:%d:%d\n", hora, min, seg);
+    }
+
+    public String getHora() {
+        return hora + ":" + min + ":" + seg;
+    }
+}
