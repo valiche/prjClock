14b5f6de5ed9d17895be29a314bc9cd081b9aefa
valiche <987266071@itaud.des.ihf>
2020-07-31 18:02:43 -0300

    bugs

--- AppRelogio.java
+++ AppRelogio.java
@@ -1,8 +1,15 @@
 public class AppRelogio {
     public static void main(String[] args) {
         Relogio clock;
-        clock=new Relogio();
-        clock.setHora();
-        //clock.getHora();
+        clock=new Relogio(17,42,00);
+        clock.getHora();
+        clock.setHora(18);
+        System.out.println(clock.getTime());
+        clock.setMin(70);
+        System.out.println(clock.getTime());
+        clock.setSeg(30);
+        System.out.println(clock.getTime());
+        clock.contHora(22,55,0);
+        System.out.println(clock.getTime());
     }
 }
--- Relogio.java
+++ Relogio.java
@@ -3,30 +3,51 @@ import java.util.Scanner;
 public class Relogio {
     int hora = 0, min = 0, seg = 0;
 
-    public void setHora() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Digite hora minuto segundo:");
-        hora = in.nextInt();
-        min = in.nextInt();
-        seg = in.nextInt();
-        for (; hora <=24; hora++) {
-            for (; min < 60; min++) {
-                for (; seg < 60; seg++){
-                    System.out.printf("%d:%d:%d\n", hora, min, seg);
-                    //public String getTime(){return hora + ":" + min + ":" + seg;}
-                }
-                    seg=0;
-            }
-            min=0;
-        }
-        hora=0;
-        min=0;
-        seg=0;
-        //System.out.printf("%d:%d:%d\n", hora, min, seg);
+    public Relogio(int hora, int min, int seg) {
+        this.hora = hora;
+        this.min = min;
+        this.seg = seg;
     }
 
     public void getHora() {
         System.out.printf("%d:%d:%d\n", hora, min, seg);
+    }
+
+    public String getTime() {
+        return hora + ":" + min + ":" + seg;
+    }
+
+    public void setHora(int hora){
+        if(hora>=0 && hora<24){
+            this.hora = hora;
+        }
+    }
+
+    public void setMin(int min){
+        if(min>=0 && min<60){
+            this.min= min;
+        }
+    }
+
+    public void setSeg(int seg){
+        if(seg>=0 && seg<60){
+            this.seg= seg;
+        }
+    }
+
+    public void contHora(int hora, int min, int seg) {
+        for (; hora < 24; hora++) {
+            for (; min < 60; min++) {
+                for (; seg < 60; seg++){
+                    //System.out.printf("%d:%d:%d\n", hora, min, seg);
+                }
+                seg = 0;
+            }
+            min = 0;
+        }
+        hora = 0;
+        min = 0;
+        seg = 0;
     }
 
 }
